fe4ba6c4f2b1e8fcb8f00e9ffd76ffae48bbd665
ianlumb1 <106744534+ianlumb1@users.noreply.github.com>
2022-11-14 13:00:24 +0000

    Add files via upload
    Updated app.py file to fix error with metrics

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,13 +1,21 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
+
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
+
     return connection
 
 # Function to get a post using its ID
@@ -17,6 +25,14 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
+
+# Function to get all posts
+def get_posts():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+
+    return posts
 
 # Define the Flask application
 app = Flask(__name__)
@@ -36,13 +52,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.error("POST NOT FOUND")
       return render_template('404.html'), 404
     else:
+      app.logger.info("Article \"%s\" retrieved!"%(post['title']))
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("About Us retrieved!")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,6 +79,8 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+
+            app.logger.info("Post \"%s\" was created"%(title))
 
             return redirect(url_for('index'))
 
@@ -101,4 +122,5 @@ def init_logger():
 
 # start the application on port 3111
 if __name__ == "__main__":
+   init_logger()
    app.run(host='0.0.0.0', port='3111')
